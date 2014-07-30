abe0ed7299b3009d6eb8fc0410363581034a4748
Marcus Comiter <marcuscomiter@Marcuss-MacBook-Pro.local>
2014-07-29 23:36:27 -0400

    Modal for introduction on sign in page

--- www/js/controllers/sign-in.js
+++ www/js/controllers/sign-in.js
@@ -1,5 +1,5 @@
 angular.module('starter.controllers')
-.controller('SignInCtrl', function($scope, $state, $firebaseSimpleLogin) {
+.controller('SignInCtrl', function($scope, $state, $firebaseSimpleLogin, $ionicModal) {
 
   var ref = new Firebase("https://omates.firebaseio.com/");
   $scope.auth = $firebaseSimpleLogin(ref);
@@ -24,4 +24,48 @@ angular.module('starter.controllers')
     user.password = "";
   };
   
+$ionicModal.fromTemplateUrl('intromodal.html', function(modal) {
+    $scope.introModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+  // Open our new task modal
+  $scope.openIntro = function() {
+    $scope.introModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeIntro = function() {
+    $scope.introModal.hide();
+  };
+
+
+  // $ionicModal.fromTemplateUrl('intromodal.html', {
+  //     scope: $scope,
+  //     animation: 'slide-in-up'
+  //   }).then(function(modal) {
+  //     $scope.modal = modal;
+  //   });
+  //   $scope.openModal = function() {
+  //     console.log('opening')
+  //     $scope.modal.show();
+  //   };
+  //   $scope.closeModal = function() {
+  //     $scope.modal.hide();
+  //   };
+  //   //Cleanup the modal when we're done with it!
+  //   $scope.$on('$destroy', function() {
+  //     $scope.modal.remove();
+  //   });
+  //   // Execute action on hide modal
+  //   $scope.$on('modal.hidden', function() {
+  //     // Execute action
+  //   });
+  //   // Execute action on remove modal
+  //   $scope.$on('modal.removed', function() {
+  //     // Execute action
+  //   });
+
+
 });
